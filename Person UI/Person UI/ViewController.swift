b8bf3280f99771eb3c6ce105f6da80ecb7cf6441
Joshua McEwen <jamyt7@mail.missouri.edu>
2019-02-15 07:27:21 -0600

    added project

---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Person UI
+//
+//  Created by McEwen, Joshua A. (MU-Student) on 2/15/19.
+//  Copyright © 2019 McEwen, Joshua A. (MU-Student). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
